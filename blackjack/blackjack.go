f66c6651210f3bc618ba63e7be2fc5842a4bce20
Atte Moisio <attemoisio@attemoisio.fi>
2024-05-10 10:00:32 +0300

    Complete blackjack (learning exercise)

---
+++ blackjack/blackjack.go
@@ -0,0 +1,69 @@
+package blackjack
+
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+	switch card {
+	case "one":
+		return 1
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	case "ten":
+		return 10
+	case "jack":
+		return 10
+	case "queen":
+		return 10
+	case "king":
+		return 10
+	case "ace":
+		return 11
+	default:
+		return 0
+	}
+}
+
+// FirstTurn returns the decision for the first turn, given two cards of the
+// player and one card of the dealer.
+func FirstTurn(card1, card2, dealerCard string) string {
+	const (
+		split = "P"
+		hit   = "H"
+		stand = "S"
+		win   = "W"
+	)
+
+	playerSum := ParseCard(card1) + ParseCard(card2)
+	dealerCardValue := ParseCard(dealerCard)
+
+	if card1 == "ace" && card2 == "ace" {
+		return split
+	} else if playerSum == 21 {
+		if dealerCardValue < 10 {
+			return win
+		}
+		return stand
+	} else if playerSum <= 11 {
+		return hit
+	} else if playerSum <= 16 {
+		if dealerCardValue >= 7 {
+			return hit
+		}
+	}
+
+	return stand
+
+}
